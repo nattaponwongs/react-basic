9646afbc1b1c9502022a811dc7da165673a37cbf
Nattapon <nattapon.wongs@bumail.net>
2022-06-09 13:02:09 +0700

    react JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Nattapon
-        </a>
-      </header>
-    </div>
+    <section>
+      <article>
+        <h1>โปรแกรมบัญชี - รายจ่าย</h1>
+        <p>บันทึกข้อมูลรายละเอียดในแต่ละวัน</p>
+        <ul>
+          <li>ค่าเดินทาง <span>-200</span></li>
+          <li>เงินเดือน <span>+20000</span></li>
+          <li>ค่าอาหาร <span>-500</span></li>
+        </ul>
+      </article>
+    </section>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HelloComponent from './components/HelloComponent'
+// import HelloComponent from './components/HelloComponent'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloComponent />
+    <App />
   </React.StrictMode>
 );
 
