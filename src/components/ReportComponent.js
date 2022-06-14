6579cac87d48ac4c2eae8016d7726007bdbe110f
Nattapon <nattapon.wongs@bumail.net>
2022-06-14 13:02:07 +0700

    format number

--- src/components/ReportComponent.js
+++ src/components/ReportComponent.js
@@ -4,18 +4,21 @@ import './ReportComponent.css'
 
 const ReportComponent = () => {
     const {income, expense} = useContext(DataContext)
+    const formatNumber = (num) => {
+        return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g,'$1,')
+    }
     return (
         <div className="balance">
             <h4>ยอดคงเหลือ (บาท) :</h4>
-            <h1>{income - expense} ฿</h1>
+            <h1>{formatNumber((income - expense).toFixed(2))} ฿</h1>
             <div className="report-container">
                 <div>
                     <h4>ยอดรายได้ทั้งหมด</h4>
-                    <p className="report plus">{income} ฿</p>
+                    <p className="report plus">{formatNumber(income)} ฿</p>
                 </div>
                 <div>
                     <h4>ยอดรายจ่ายทั้งหมด</h4>
-                    <p className="report minus">{expense} ฿</p>
+                    <p className="report minus">{formatNumber(expense)} ฿</p>
                 </div>
             </div>
         </div>
